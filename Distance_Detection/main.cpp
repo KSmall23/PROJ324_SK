0344e83c8c2346b86e56521a3fecd03a5bfb0607
KSmall23 <70811911+KSmall23@users.noreply.github.com>
2023-02-16 16:30:44 +0000

    can't remember...

--- Distance_Detection/main.cpp
+++ Distance_Detection/main.cpp
@@ -16,7 +16,8 @@ double Fs = 80000.0;
 double T = 1.0/Fs;
 uint32_t n = 0;
 
-//AnalogOut CarrierWave (DAC1_AN_PIN);
+DigitalIn Bluebutton (USER_BUTTON);
+AnalogOut CarrierWave (DAC1_AN_PIN);
 //AnalogOut CarrierWave2 (DAC2_AN_PIN);
 
 Semaphore sem(1);
@@ -29,23 +30,37 @@ void trh();
 // main() runs in its own thread in the OS
 int main()
 {
-    	RCC->AHB1ENR|=RCC_AHB1ENR_GPIOAEN;			//DAC port clock enable
-	GPIOA->MODER|=(3u<<(2*4));			//DAC output pin set as anaglogue
-    	GPIOA->MODER|=(3u<<(2*5));			//DAC output pin set as anaglogue
+//     	RCC->AHB1ENR|=RCC_AHB1ENR_GPIOAEN;			//DAC port clock enable
+// 	GPIOA->MODER|=(3u<<(2*4));			//DAC output pin set as anaglogue
+//     	GPIOA->MODER|=(3u<<(2*5));			//DAC output pin set as anaglogue
 	
-	RCC->APB1ENR|=RCC_APB1ENR_DACEN;				//DAC clock enable
-	DAC->CR|=DAC_CR_EN2;										//DAC 2 enabled
+// 	RCC->APB1ENR|=RCC_APB1ENR_DACEN;				//DAC clock enable
+// 	DAC->CR|=DAC_CR_EN2;										//DAC 2 enabled
 
-	DAC->CR|=DAC_CR_EN1;										//DAC 2 enabled
+// 	DAC->CR|=DAC_CR_EN1;										//DAC 2 enabled
 
-    Ticker Tick;
-   // t1.set_priority(osPriorityRealtime);
-   // t1.start(trh);
-   Tick.attach_us(Teest,12);
+//     Ticker Tick;
+//    // t1.set_priority(osPriorityRealtime);
+//    // t1.start(trh);
+//    Tick.attach_us(Teest,12);
      while (true) { 
-       // std::chrono::duration(1us);
-       
-    ThisThread:: sleep_for(osWaitForever);
+     while (Bluebutton ==0);  // std::chrono::duration(1us);
+     ThisThread:: sleep_for(10ms);
+     while (Bluebutton == 1);
+     ThisThread:: sleep_for(10ms);
+     for (int n = 0; n<100000; n++)
+     {
+         CarrierWave = 0.33;
+     }
+     
+    //  for (int n = 0; n<1000000; n++)
+    //  {
+          CarrierWave =0;
+    //  } 
+  
+  
+  
+   // ThisThread:: sleep_for(osWaitForever);
 //    CarrierWave = 0.5f + 0.5f*0.5*sin(2*pi*Freq*T*n);
 //          n = (n == 999999) ? 0 : (n+1);
  
@@ -54,10 +69,10 @@ int main()
 
 void Teest()
 {
-            uint16_t cw = 0;//+ (4095*0.5*sin(2*pi*Freq*T*n));
-                     n = (n == 100) ? 0 : (n+1);
-        DAC->DHR12R2 = cw;
-        DAC->DHR12R1 = cw;
+          //  uint16_t cw = 0;//+ (4095*0.5*sin(2*pi*Freq*T*n));
+           //          n = (n == 100) ? 0 : (n+1);
+        // DAC->DHR12R2 = cw;
+        // DAC->DHR12R1 = cw;
    // sem.release();
 // t1.flags_set(1);
 }
@@ -71,7 +86,7 @@ void trh()
   //      uint32_t cw = 2048;//+ (4095*0.5*sin(2*pi*Freq*T*n));
     //    DAC1->DHR12R1 = cw;
        //  CarrierWave = 0.5f + 0.5f*0.5*sin(2*pi*Freq*T*n);
-         n = (n == 100) ? 0 : (n+1);
+      //   n = (n == 100) ? 0 : (n+1);
 
 
     } 
--- ELEC240 Coursework Final/main.c
+++ ELEC240 Coursework Final/main.c
@@ -1,5 +1,6 @@
 #include <stm32f4xx.h>
 #include <math.h>
+#include <stdio.h>
 #include "PLL_Config.c"
 #include "ADC.c"            //holds functions relating to ADC
 #include "DAC.c"            //holds functions relating to DAC
@@ -15,16 +16,7 @@
 //#include "traffic_lights.c" //holds functions for the traffic light system
 
 
-//unsigned short POT_D; //global variable to store reading on POT
-//unsigned short LDR_D; //global variable to store reading on LDR
-//unsigned short MIC_D; //global variable to store reading on MIC
 
-//double POT_V; //global variable to store voltage of POT
-//double LDR_V; //global variable to store voltage of LDR
-//double MIC_V; //global variable to store voltage of MIC
-
-//static int LCD_display=1;  //global variable to control which voltage is displayed on LCD (POT, MIC, LDR)
- double y[6800];
 
 int main(void)
 {
@@ -43,37 +35,35 @@ int main(void)
 //	Init_Timer7_10sec(); //config timer 7 to interrupt every 10 seconds
 //	InitBlueButton_EXTI(); //config external interrupt to serve blue button
 //	Init_Timer5_buzzer_off_low(); //config timer 5 for turning the buzzer off
-	int n =0;
-for(n=0; n<6800; n++)
-{
-	double x = sin(2*3.14*40400*0.000000024*n);
-  y[n] = x;
-	return y[n];
-};
-
 	Init_Timer2_dac_sin();
 	while(1)
-	{}
+	{
+
+	}
 }
-
-
-
-
 
 	
 
 void TIM2_IRQHandler(void)			//TIMER 2 INTERRUPT SERVICE ROUTINE for outputing to DACs, varied speeds depending on which wave its outputing 
 {
 TIM2->SR&=~TIM_SR_UIF;				//clear interrupt flag in status register 	
-static int count=0;           //create static variable 'count' to go through arrays
+//static int count=0;           //create static variable 'count' to go through arrays
+static int count1=0;  
 	                      //output=3 when C has been typed or pressed, gives sine wave on DAC2
-  output_dac(0.5f + y[count]);   //output=3 when C has been typed or pressed, gives sine wave on DAC2 
+  //output_dac(carry[count]);   //output=3 when C has been typed or pressed, gives sine wave on DAC2 
+	output_dac1(square[count1]);
 
-	count=count+99;  //goes through arrays at incrementy of ten steps
+//	count=count+99;  //goes through arrays at incrementy of ten steps
+	count1=count1+15;  //goes through arrays at incrementy of ten steps
   
-	if(count>6799) //reset count value to 0 once array gone through completely
+//	if(count>6799) //reset count value to 0 once array gone through completely
+//   {            //reset count value to 0 once array gone through completely
+//   count=0;     //reset count value to 0 once array gone through completely
+//   }		        //reset count value to 0 once array gone through completely
+	 
+	 if(count1>998) //reset count value to 0 once array gone through completely
    {            //reset count value to 0 once array gone through completely
-   count=0;     //reset count value to 0 once array gone through completely
+   count1=0;     //reset count value to 0 once array gone through completely
    }		        //reset count value to 0 once array gone through completely
 }
 
